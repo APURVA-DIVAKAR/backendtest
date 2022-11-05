0ec821d23d3fdb8aa215d39b4fa4566aa37b45aa
APURVA-DIVAKAR <99614708+APURVA-DIVAKAR@users.noreply.github.com>
2022-11-05 10:11:21 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+
+const express = require('express')
+
+const app = express()
+const mongoose = require('mongoose')
+const cors = require('cors')
+require('dotenv').config()
+
+app.use(cors()) 
+app.use(express.json()) 
+
+app.use(express.urlencoded({ extended: true }))
+
+// console.log(process.env.DATABASE_URL)
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+
+app.get('/',(req,res)=>res.send(`hello`))
+app.get("api/test",(req,res)=>res.send("working"))
+
+
+const db = mongoose.connection
+db.on('error', (error) => console.error(error))
+db.once('open', () => console.log('Database connected'))
+
+app.listen(process.env.PORT, () => {
+    console.log("The API is Working.")
+})
